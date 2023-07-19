f8dd6e505b1b4751a838c3f627fe0619c6477603
Orne <ornelevato@gmail.com>
2023-07-19 18:33:34 -0300

    agregando if

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 console.log ("probando 123")
 
-const usuario = "Mi nombre"
-const contrasenia = "1234"
+//const usuario = "Mi nombre"
+//const contrasenia = "1234"
 
-const PreguntarUsuario = prompt("ingrese su usuario")
-const PreguntarContraseña = prompt("ingrese su contraseña")
+//const PreguntarUsuario = prompt("ingrese su usuario")
+//const PreguntarContraseña = prompt("ingrese su contraseña")
 
-console.log( usuario === PreguntarUsuario && contrasenia === PreguntarContraseña )
+//console.log( usuario === PreguntarUsuario && contrasenia === PreguntarContraseña )
 
-if ( usuario === PreguntarUsuario && contrasenia === PreguntarContraseña ){
-    alert ("Bienvenido!")
+let nombreIngresado = prompt("Ingresar nombre")
+let apellidoIngresado = prompt("Ingresar apellido")
+
+if((nombreIngresado !="") && (apellidoIngresado !="")){
+    alert("nombre: "+nombreIngresado +"\napellido:"+apellidoIngresado);
+} else{
+    alert("error: ingresar nombre y apellido")
 }
-else {
-    alert ("contraseña o usuario incorrectos")
-}
 
+console.log(nombreIngresado +" "+ apellidoIngresado)
+//if ( usuario === PreguntarUsuario && contrasenia === PreguntarContraseña ){
+//    alert ("Bienvenido!")
+//}
+//else {
+ //   alert ("contraseña o usuario incorrectos")
+//}
+
+const numeroPar = 12
+  
+const numeroImpar = 97
+
+console.log("numeroImpar + numeroPar")
